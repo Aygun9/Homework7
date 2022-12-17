327ce61fdda8ed36a5fbc0451193b8b1ce2b3fa4
Aygun <aygun.gasimova@inbox.ru>
2022-12-17 14:39:54 +0400

    Task52 completed

---
+++ Task52/Program.cs
@@ -0,0 +1,59 @@
+﻿//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце..
+
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+void PrintArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int m = EnterNumber("Insert m:  ");
+int n = EnterNumber("Insert n:  ");
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+PrintArr(matrix);
+
+double sum = 0;
+for (int i = 0; i < matrix.GetLength(1); i++)
+{
+   sum = 0;
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        sum += matrix[j, i];
+    }
+    double average=sum/matrix.GetLength(0);
+    Console.WriteLine(Math.Round(average,1) + " ; ");
+}
+
+
+
+
+
+
